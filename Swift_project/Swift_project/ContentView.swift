f892b6853ba1d7bf7435284a165a67cb5f6188c7
agiss615 <sooin615@naver.com>
2022-06-30 19:51:24 +0900

    second commit

--- Swift_project/Swift_project/ContentView.swift
+++ Swift_project/Swift_project/ContentView.swift
@@ -5,24 +5,54 @@
 //  Created by Agiss on 2022/06/30.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     
+    // @State 값의 변화를 감지 -> 뷰에 적용
+    @State
+    private var isActivated: Bool = false
+    
     //몸체
     var body: some View {
        
-        HStack{
+        NavigationView{
+            VStack{
+                HStack{
             
-            MyVstackView()
-            MyVstackView()
-            MyVstackView()
+                    MyVstackView()
+                    MyVstackView()
+                    MyVstackView()
             
             
-        }// Hstack
-            .padding(10.0)
-            .background(Color.yellow)
-        
+                }// Hstack
+                .padding(isActivated ? 50.0 : 10.0)
+                //
+                .background(isActivated ? Color.yellow : Color.black)
+                // 탭 제스쳐 추가
+                .onTapGesture {
+                    print("HStack 이 클릭되었다.")
+                
+                    // 애니메이션과 함께
+                    withAnimation{
+                        // toggle() true 이면 false 로 false 이면 true
+                        self.isActivated.toggle()
+                    }
+                }// Hstack
+            
+                // 네비게이션 버튼(링크)
+                NavigationLink(destination: MyTextView() ){
+                    Text("네비게이션")
+                        .font(.system(size: 40))
+                        .fontWeight(.bold)
+                        .padding()
+                        .background(Color.orange)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(30)
+                }
+            }
+        }// NavigationView
         
     }
 }
---
+++ Swift_project/Swift_project/MyTextView.swift
@@ -0,0 +1,58 @@
+//
+//  MyTextView.swift
+//  Swift_project
+//
+//  Created by Agiss on 2022/06/30.
+//
+
+import SwiftUI
+
+struct MyTextView: View {
+    
+    // @State 값의 변화를 감지 -> 뷰에 적용
+    @State
+    private var index: Int = 0
+    
+    // 배경색 배열 준비
+    private let backgroundColors = [
+        Color.red,
+        Color.yellow,
+        Color.blue,
+        Color.cyan,
+        Color.gray
+    ]
+    
+    var body: some View {
+        VStack{
+            
+            Spacer()
+            
+            Text("배경 아이템 인덱스 \(self.index)")
+                .font(.system(size: 30))
+                .fontWeight(.bold)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            
+            Spacer()
+            
+        }.background(backgroundColors[index])
+         .edgesIgnoringSafeArea(.all)
+         .onTapGesture {
+             print("배경아이템이 클릭 되었다.")
+             
+             if(self.index == self.backgroundColors.count - 1){
+                 self.index = 0
+             } else {
+                 self.index += 1
+             }
+             
+             
+         }
+    }
+}
+
+
+struct MyTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyTextView()
+    }
+}
